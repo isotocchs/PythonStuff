f9175ae97e7de93740f6aa212e6fda9520dafeda
isotocchs <89474695+isotocchs@users.noreply.github.com>
2023-03-30 08:43:34 -0400

    save

--- guiStuff.py
+++ guiStuff.py
@@ -2,15 +2,16 @@
 import PySimpleGUI as sg
 
 title = "Conversion"
-spinList = ["item1", "item2","item3", "item4"]
+spinList = ["in to ft", "ft to in","meter to ft", "ft to meter"]
 
 layout = [
-    [sg.Text("Test", font=("Arial",45), enable_events=True, key="TextBox1", text_color="Blue"),
-    sg.Spin(spinList, size=(10,3),font=("Arial",45), key="ItemSpin")],
-    [sg.Button("OK", font=("Arial",45), key="Button1")],
-    [sg.Input(font=("Arial",45), size=(20,1), background_color="Teal", key="InputBox1")],
-    [sg.Button("OK", font=("Arial",45), key="Button2")],
-    [sg.Button("Change Element", font=("Arial",45), key="Button3")]
+    [sg.Input(font=("Arial",45), size=(20,1), background_color="Green", key="InputBox1"),
+    sg.Spin(spinList, size=(10,3),font=("Arial",45), key="ItemSpin"), 
+    sg.Button("Convert", font=("Arial",45), key="Button1")],
+    [sg.Text("Test", font=("Arial",45), enable_events=True, key="TextBox1", text_color="Blue")]
+    # [sg.Input(font=("Arial",45), size=(20,1), background_color="Teal", key="InputBox2")],
+    # [sg.Button("OK", font=("Arial",45), key="Button3")],
+    # [sg.Button("Change Element", font=("Arial",45), key="Button4")]
 
 ]
 
@@ -42,20 +43,37 @@ while True:
     if event == "Button1":
         print("-------------------")
         print("Printing the Values")
-        print(values)
+        itemSpinInfo = values["ItemSpin"]
+        inputBox1Info = values["InputBox1"]
+        inputBox2Info = values["InputBox2"]
+        if inputBox1Info.isnumeric():
+            print("Thank you for putting in a number")
+        else:
+            print("Please put in a number")
+
+        if itemSpinInfo == "item3":
+            print("I have selected Item 3")
+        else: 
+            print("I have not selected Item 3")
+        print("-------------------")
+    
+    if event == "TextBox1":
+        print("-------------------")
+        print("I've clicked on the text")
+        # print(values)
         print("-------------------")
 
     if event == "Button2":
         print("-------------------")
         print("Printing the Values from only the input box")
-        print(values["InputBox1"])
+        # print(values["InputBox1"])
         #you can save that info in a variable
-        inputBox = values["InputBox1"]
+        # inputBox = values["InputBox1"]
         #we can test that variable to make sure its what we want.
-        if inputBox.isnumeric():
-            print(inputBox)
-        else:
-            print("Please put in a number")
+        # if inputBox.isnumeric():
+        #     print(inputBox)
+        # else:
+        #     print("Please put in a number")
 
         # we can test several cases with a match case clause
         match values["ItemSpin"]:
@@ -71,11 +89,16 @@ while True:
         print("-------------------")
 
     #Updating Elements
-
-    if event == "Button3":
-        # myAppWindow["TextBox1"].update(values["InputBox1"])
-    # You can upadate other things like visibility
-        myAppWindow["TextBox1"].update(visible = False)
+#     x = 45-3
+#     if event == "Button4":
+#         # myAppWindow["TextBox1"].update(x)
+# # You can upadate other things like visibility
+#         myAppWindow["TextBox1"].update(visible = False)
+    
+#     if event == "Button3":
+#         # myAppWindow["TextBox1"].update(x)
+# # You can upadate other things like visibility
+#         myAppWindow["TextBox1"].update(visible = True)
 
 
 
